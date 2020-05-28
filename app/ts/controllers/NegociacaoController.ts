9bfa7b96a0c251873b65792b0fe5cc1f446d73ba
Anderson Mendes de Souza <amendes@server01.foursys.local>
2020-05-28 15:17:32 -0300

    Organizando o codigo com namespace

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,13 +1,16 @@
 class NegociacaoController {
-    private _inputData: HTMLInputElement;
-    private _inputQuantidade: HTMLInputElement;
-    private _inputValor: HTMLInputElement;
-
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new Views.NegociaoesView('#negociacoesView');
+    private _mensagemView = new Views.MensagemView('#mensagemView');
 
     constructor() {
-        this._inputData = <HTMLInputElement>document.querySelector('#data');
-        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
-        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
     /*
         adiciona(event:Event){
@@ -30,10 +33,27 @@ class NegociacaoController {
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            new Date(this._inputData.value.replace(/-/g, '-')),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value));
-        console.log(negociacao);
 
+            new Date(this._inputData.val().replace(/-/g, '-')),
+
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val()));
+
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!!');
+
+        //this._negociacoesView.update();
+        /*
+                this._negociacoes.retornaArray().length=0;
+        
+                this._negociacoes.retornaArray().forEach(negociacao=>{
+                    console.log(negociacao.data);
+                    console.log(negociacao.quantidade);
+                    console.log(negociacao.valor);
+                });
+                //console.log(this._negociacoes.retornaArray());
+        
+        */
     }
 }
